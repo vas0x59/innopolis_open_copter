f8311087c427c990b3da5950a3ad0defafe413c7
Vas2016 <vasily.yuryev.a1@gmail.com>
2019-05-05 11:20:25 +0700

    map

--- tests/map_gen.py
+++ tests/map_gen.py
@@ -13,12 +13,12 @@ for i in range(co):
     map_arr.append(a)
 
 # print(map_arr)
-f = open("out.txt", "w+")
+f = open("map_gen_1.txt", "w+")
 for i in range(len(map_arr)-1, -1,  -1): 
     for j in range(len(map_arr[i])-1,-1, -1): 
         x = round(j*0.3, 2)
         y = round(i*0.3, 2)
-        f.write(str(str(map_arr[i][j]) + "\t" + str(x)  + "\t" + str(y)  + "\t0\t0\t0\t0\n"))
+        f.write(str(str(map_arr[i][j])+ "\t" + str(mark_size) + "\t" + str(x)  + "\t" + str(y)  + "\t0\t0\t0\t0\n"))
         map_arr[i][j] = (map_arr[i][j], mark_size, x, y, 0, 0, 0, 0)
         
 f.close()
