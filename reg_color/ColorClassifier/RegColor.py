f0642ef63466208e1d5bdaed07b96e81e96a04ad
vas59 <vasily.yuryev.a1@gmail.com>
2019-06-02 21:35:30 +0700

    Reg COLORS

---
+++ reg_color/ColorClassifier/RegColor.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+labels = ["red", "yellow", "green", "blue", "none"]
+colors = {
+    "red":(20, 10, 80), "yellow":(0, 125, 125), "green":(70, 100, 60), "blue":(100, 150, 180)
+}
+
+def regSum(img):
+    color = "none"
+    #bgr
+    r_mean = np.mean(img[:, :, 2]) 
+    g_mean= np.mean(img[:, :, 1]) 
+    b_mean = np.mean(img[:, :, 0]) 
+    a = np.array((b_mean ,g_mean, r_mean))
+    max_c = "none"
+    max_d = 0
+    for i in colors:
+        b = np.array(colors[i])
+        dist = 1-np.linalg.norm(a-b) / 255
+        # print(dist)
+        if dist > 0.6:
+            if dist  > max_d:
+                max_c = i
+                max_d = dist
+    color = max_c
+    return color
+
+def regConturs(img):
+    color = "none"
+    return color
--- reg_color/test.py
+++ reg_color/test.py
@@ -1,12 +1,13 @@
-import RegColor as rc
+import ColorClassifier.RegColor as rc
 import cv2
 import numpy as np 
 
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(2)
 
 
 while cv2.waitKey(1) != ord('q'):
     ret, frame = cap.read()
     color = rc.regSum(frame)
+    cv2.imshow("frame", frame)
     print(color)
