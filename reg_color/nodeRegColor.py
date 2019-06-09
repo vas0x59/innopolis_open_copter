62f75aadb14c6e4235e4b15574b3e44d01ff3248
vas59 <vasily.yuryev.a1@gmail.com>
2019-06-09 21:23:46 +0700

    ROS

--- reg_color/nodeRegColor.py
+++ reg_color/nodeRegColor.py
@@ -1,6 +1,24 @@
-from RegColor import RegColor
+from ColorClassifier import RegColor
 import rospy
 import cv2
 import numpy as np 
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 
+rospy.init_node('regColor')
+# image_pub = rospy.Publisher("image_topic_2",Image)
+def callback(data):
+    try:
+      cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    (rows,cols,channels) = cv_image.shape
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(3)
+bridge = CvBridge()
+image_sub = rospy.Subscriber("image_topic",Image,callback)
+
+rospy.spin()
