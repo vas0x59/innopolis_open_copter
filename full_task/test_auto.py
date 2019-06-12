e09c7dc68bddce04731100bd58bf4ee1c41438f5
MagicWinnie <magicwinnie@yandex.ru>
2019-06-12 17:41:28 +0700

    test flight

--- full_task/test_auto.py
+++ full_task/test_auto.py
@@ -7,25 +7,30 @@ import time
 
 import Utils
 
-points = {"takeoff":(2.76, 2, 1.5), "land": (0.33, 2, 1.5)}
+points = {"takeoff":(2.76, 1.5, 1.3), "land": (0.33, 1.5, 1.3)}
 
 magnit = Utils.Magnet()
 magnit.off()
 
 copter = Utils.Copter(markers_flipped=True)
 copter.start_coord = points["takeoff"]
-copter.zero_z()
+copter.callib_zero_z()
 
 copter.takeoff(1.5)
+print("takeoff compl")
 rospy.sleep(0.5)
 
-
+print("go to tk point")
 copter.go_to_point(points["takeoff"])
+print("hold tk point")
 rospy.sleep(3)
+
+print("go to tk land")
 copter.go_to_point(points["land"])
+print("hold tk land")
 rospy.sleep(3)
 # magnit.off()
-
+print("land")
 copter.land()
 # copter.go_to_point((0, 0, 1.8))
 
