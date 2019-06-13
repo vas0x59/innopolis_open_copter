174dd6f830ab816816aa2627c55d23563d7ee07e
vas59 <vasily.yuryev.a1@gmail.com>
2019-06-13 15:24:05 +0700

    functions

--- full_task/obstacles.py
+++ full_task/obstacles.py
@@ -6,7 +6,57 @@ import math
 import time
 import Utils
 
-points = {"takeoff":(2.3, 2, 1.3),"ring":(2.8, 1.4, 0.5),"gate":(1.9, 0.5, 0.5),"land": (0.27, 2.05, 1.3)}
+points = {
+    "takeoff":(2.3, 2, 1.3),
+    # "ring":(2.8, 1.4, 0.5),
+    # "gate":(1.9, 0.5, 0.5),
+    "land": (0.27, 2.05, 1.3)
+}
+gate_points = {
+    "gate_1":(2.2, 0.5, 0.5),
+    "gate_2":(1.4, 0.5, 0.5)
+}
+grab_points = {
+    "grab_hover":(1, 1,0.4)
+    # "grab":(1, 1, 0.4)
+}
+ring_points = {
+    "ring_1":(2.8, 1.7, 0.5),
+    "ring_2":(2.8, 0.8, 0.5)
+}
+def gate():
+    print("going to gate_1")
+    copter.go_to_point(gate_points["gate_1"])
+    print("going to gate_2")
+    copter.go_to_point(gate_points["gate_2"])
+    print("gate done")
+
+def takeoff():
+    copter.takeoff(1.5)
+    print("takeoff compl")
+    rospy.sleep(0.5)
+
+    print("go to tk point")
+    copter.go_to_point(points["takeoff"])
+    print("hold tk point")
+    rospy.sleep(3)
+
+def ring():
+    print("going to ring_1")
+    copter.go_to_point(ring_points["ring_1"])
+    print("going to ring_2")
+    copter.go_to_point(ring_points["ring_2"])
+    print("ring done")
+def land():
+    print("go to land")
+    copter.go_to_point(points["land"])
+    print("hold land")
+    rospy.sleep(3)
+    print("land")
+    copter.land()
+
+# def grab():
+
 corners = {"upper-right":(2.4, 0.5, 0.5), "upper-left":(0.2, 0.2, 0.5)}
 
 # ros_tools = Utils.RosTools()
@@ -18,29 +68,7 @@ copter.start_coord = points["takeoff"]
 copter.zero_z = 2.5
 # copter.callib_zero_z()
 
-copter.takeoff(1.5)
-print("takeoff compl")
-rospy.sleep(0.5)
-
-print("go to tk point")
-copter.go_to_point(points["takeoff"])
-print("hold tk point")
-rospy.sleep(3)
-
-print("going to ring")
-copter.go_to_point(points["ring"])
-
-copter.go_to_point(corners["upper-right"])
-print("going to gate")
-
-copter.go_to_point(points["gate"])
-print("going to landing point")
-
-copter.go_to_point(corners["upper-left"])
-
-print("go to tk land")
-copter.go_to_point(points["land"])
-print("hold tk land")
-rospy.sleep(3)
-print("land")
-copter.land()
+takeoff()
+ring()
+gate()
+land()
