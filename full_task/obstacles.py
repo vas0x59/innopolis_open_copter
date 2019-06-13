fe86eafc0894de8db15911288e8d813286e0e402
vas59 <vasily.yuryev.a1@gmail.com>
2019-06-13 13:21:40 +0700

    rospy init

--- full_task/obstacles.py
+++ full_task/obstacles.py
@@ -9,6 +9,10 @@ import Utils
 points = {"takeoff":(2.3, 2, 1.3),"ring":(2.8, 1.4, 0.5),"gate":(1.9, 0.5, 0.5),"land": (0.27, 2.05, 1.3)}
 corners = {"upper-right":(2.4, 0.5, 0.5), "upper-left":(0.2, 0.2, 0.5)}
 
+# ros_tools = Utils.RosTools()
+
+rospy.init_node("flight")
+
 copter = Utils.Copter(markers_flipped=True)
 copter.start_coord = points["takeoff"]
 copter.zero_z = 2.5
--- full_task/test_auto.py
+++ full_task/test_auto.py
@@ -4,13 +4,14 @@ from std_srvs.srv import Trigger
 from mavros_msgs.srv import CommandBool
 import math
 import time
-
 import Utils
 
 points = {"takeoff":(2.3, 2, 1.3), "land": (0.27, 2.05, 1.3)}
 
 magnit = Utils.Magnet()
 magnit.off()
+
+rospy.init_node("flight")
 
 copter = Utils.Copter(markers_flipped=True)
 copter.start_coord = points["takeoff"]
@@ -35,4 +36,4 @@ rospy.sleep(3)
 print("land")
 copter.land()
 # copter.go_to_point((0, 0, 1.8))
-
+# rospy.spin()
