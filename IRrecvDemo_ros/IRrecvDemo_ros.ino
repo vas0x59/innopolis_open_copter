5601fe5f6344da21b2fd36ae203d0622169372fa
vas59 <vasily.yuryev.a1@gmail.com>
2019-06-17 01:54:42 +0700

    Arduino code

---
+++ IRrecvDemo_ros/IRrecvDemo_ros.ino
@@ -0,0 +1,65 @@
+#include <IRremote.h>
+//#include <IRremoteESP8266.h>
+//#include <IRrecv.h>
+//#include <IRutils.h>
+#include <ros.h>
+#include <std_msgs/String.h>
+int RECV_PIN = 3;
+int POWER_PIN = 2;
+
+ros::NodeHandle  nh;
+
+std_msgs::String str_msg;
+ros::Publisher chatter("ir_reciv", &str_msg);
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+//  Serial.begin(57600);
+  // In case the interrupt driver crashes on setup, give a clue
+  // to the user what's going on.
+  //  Serial.println("Enabling IRin");
+  pinMode(POWER_PIN, OUTPUT);
+  digitalWrite(POWER_PIN, HIGH);
+  irrecv.enableIRIn(); // Start the receiver
+  nh.initNode();
+  nh.advertise(chatter);
+  //  Serial.println("Enabled IRin");
+}
+int i_count = 0;
+bool qwe = false;
+String last_ir = "none";
+void loop() {
+  if (irrecv.decode(&results)) {
+
+    //    int v = ;
+    String str = String((int)results.value, HEX);
+    str_msg.data = str.c_str();
+    chatter.publish( &str_msg );
+    last_ir = str;
+    irrecv.resume(); // Receive the next value
+    qwe = true;
+    i_count = 0;
+  }
+  else if (qwe) {
+    if (i_count < 1) {
+      str_msg.data = last_ir.c_str();
+      chatter.publish( &str_msg );
+      i_count++;
+    }
+    else {
+      qwe = false;
+      i_count = 0;
+    }
+
+  }
+  else {
+    str_msg.data = "none";
+    chatter.publish( &str_msg );
+  }
+  nh.spinOnce();
+  delay(300);
+}
